a873787600b73c91ff6abdb6d0ae0451532e35a4
Emre Kisaboyun <emre.kisaboyun@retmind.com>
2022-04-19 00:32:24 +0300

    exception log aspect giris

---
+++ Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
@@ -0,0 +1,29 @@
+﻿using Castle.DynamicProxy;
+using Core.CrossCuttingConcerns.Logging.Log4Net;
+using Core.Utilities.Interceptors;
+using Core.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Aspects.Autofac.Exception
+{
+    public class ExceptionLogAspect : MethodInterception
+    {
+        private LoggerServiceBase _loggerServiceBase;
+        public ExceptionLogAspect(Type loggerService)
+        {
+            if (loggerService.BaseType != typeof(LoggerServiceBase))
+            {
+                throw new System.Exception(AspectMessages.WrongLoggerType);
+            }
+            _loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggerService);
+        }
+        protected override void OnException(IInvocation invocation)
+        {
+            base.OnException(invocation);
+        }
+    }
+}
