7d85e548b64b31a7e4f47503c852135b0c97b265
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-07-18 15:56:10 +0300

    Commit line added

--- Core/Aspects/Autofac/Caching/CacheAspect.cs
+++ Core/Aspects/Autofac/Caching/CacheAspect.cs
@@ -22,7 +22,8 @@ namespace Core.Aspects.Autofac.Caching
             var methodName = string.Format($"{invocation.Method.ReflectedType.FullName}.{invocation.Method.Name}");
             var arguments = invocation.Arguments.ToList();
             var key = $"{methodName}({string.Join(",", arguments.Select(x => x?.ToString() ?? "<Null>"))})";
-            // Bu hale getirmiş olduk örn;  --> ProductManager.GetByCategory(1)
+            // Bu hale getirmiş olduk örn;  -->
+            // Product.GetAll() / ProductManager.GetByCategory(1) / ProductManager.GetXYZ(1, "asd", 67 , 1) ...
             if (_cacheManager.IsAdd(key))
             {
                 // Cache' deyse buraya düşecek ve değerler çok daha hızlı bir şekilde gelmiş olacak.
