53f88c970657edd0cf0d576a2458909b0aa096f4
Emre Kisaboyun <emre.kisaboyun@bs.nttdata.com>
2022-09-15 14:37:56 +0300

    ProductController added to WvcWebUI layer.

---
+++ MvcWebUI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Business.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using MvcWebUI.Models;
+
+namespace MvcWebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public IActionResult GetList()
+        {
+            var model = new ProductListViewModel
+            {
+                Products = _productService.GetList().Data
+            };
+            return View(model);
+        }
+    }
+}
--- MvcWebUI/Startup.cs
+++ MvcWebUI/Startup.cs
@@ -1,12 +1,15 @@
+using Core.DependencyResolvers;
+using Core.Extensions;
+using Core.Utilities.IoC;
+using Core.Utilities.Security.Encyption;
+using Core.Utilities.Security.Jwt;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 
 namespace MvcWebUI
 {
@@ -19,13 +22,39 @@ namespace MvcWebUI
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //services.AddCors(options =>
+            //{
+            //    // localhost:3000 ' e izin ver dedik. 3000' i random verdik.
+            //    options.AddPolicy("AllowOrigin",
+            //                        builder => builder.WithOrigins("http://localhost:3000"));
+            //});
+
+            //var tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            //        .AddJwtBearer(options =>
+            //        {
+            //            options.TokenValidationParameters = new TokenValidationParameters
+            //            {
+            //                ValidateIssuer = true,
+            //                ValidateAudience = true,
+            //                ValidateLifetime = true,
+            //                ValidIssuer = tokenOptions.Issuer,
+            //                ValidAudience = tokenOptions.Audience,
+            //                ValidateIssuerSigningKey = true,
+            //                IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey),
+            //            };
+            //        });
+
+            services.AddDependencyResolvers(new ICoreModule[]
+            {
+                new CoreModule()
+            });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -38,7 +67,13 @@ namespace MvcWebUI
             }
             app.UseStaticFiles();
 
+            app.ConfigureCustomExceptionMiddleware();
+
+            //app.UseCors(builder => builder.WithOrigins("http://localhost:3000").AllowAnyHeader());
+
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
